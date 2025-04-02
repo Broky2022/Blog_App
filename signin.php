2ea09317041316752e9218343a5e0bc076baa732
Broky2022 <nhktb2k3@gmail.com>
2025-04-03 01:50:58 +0700

    [UPDATE][signin.php & signup.php] cải thiện việc xử lý form và cải thiện user feedback

--- signin.php
+++ signin.php
@@ -1,17 +1,19 @@
+<?php
+include 'config/constains.php';
+// Check for signup errors
+$signup_error = $_SESSION['signup'] ?? null;
+unset($_SESSION['signup']);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Sign In</title>
-  <link rel="stylesheet" href="./style.css" />
-  <link
-    rel="stylesheet"
-    href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
-    rel="stylesheet" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Đăng nhập</title>
+    <link rel="stylesheet" href="<?= ROOT_URL ?>css/style.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -22,7 +24,7 @@
         <input type="text" placeholder="Username or Email" />
         <input type="password" placeholder="Password" />
         <button type="submit" class="btn">Sign In</button>
-        <small>Don't have an account? <a href="signup.html">Sign Up</a></small>
+        <small>Don't have an account? <a href="signup.php">Sign Up</a></small>
       </form>
     </div>
   </section>
--- signup.php
+++ signup.php
@@ -1,36 +1,62 @@
+<?php
+include 'config/constains.php';
+// Check for signup errors
+$signup_error = $_SESSION['signup'] ?? null;
+
+// lấy lại thông tin đã nhập trong form
+$firstname = $_SESSION['signup-data']['firstname'] ?? null;
+$lastname = $_SESSION['signup-data']['lastname'] ?? null;
+$username = $_SESSION['signup-data']['username'] ?? null;
+$email = $_SESSION['signup-data']['email'] ?? null;
+$createpassword = $_SESSION['signup-data']['createpassword'] ?? null;
+$confirmpassword = $_SESSION['signup-data']['confirmpassword'] ?? null;
+//$avatar = $_SESSION['signup-data']['avatar'] ?? null;
+
+// Xóa thông tin đã nhập trong form
+unset($_SESSION['signup-data']);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Sign Up</title>
-  <link rel="stylesheet" href="./style.css" />
-  <link
-    rel="stylesheet"
-    href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
-    rel="stylesheet" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Đăng ký</title>
+  <link rel="stylesheet" href="<?= ROOT_URL ?>css/style.css">
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
+
 
 <body>
   <section class="form__section">
     <div class="container form__section-container">
       <h2>Sign Up</h2>
-      <form action="" enctype="multipart/form-data">
-        <input type="text" placeholder="First Name" />
-        <input type="text" placeholder="Last Name" />
-        <input type="text" placeholder="Username" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Create Password" />
-        <input type="password" placeholder="Confirm Password" />
+      <?php if ($signup_error) : ?>
+        <div class="alert__message error">
+          <p>
+            <?= $signup_error;
+            unset($_SESSION['signup']);
+            ?>
+          </p>
+        </div>
+      <?php endif; ?>
+      <form action="<?= ROOT_URL ?>controller/signup-controller.php" enctype="multipart/form-data" method="POST">
+        <input type="text" name="firstname" value="<?= $firstname ?>" placeholder="First Name" />
+        <input type="text" name="lastname" value="<?= $lastname ?>" placeholder="Last Name" />
+        <input type="text" name="username" value="<?= $username ?>" placeholder="Username" />
+        <input type="email" name="email" value="<?= $email ?>" placeholder="Email" />
+        <input type="password" name="createpassword" value="<?= $createpassword ?>" placeholder="Create Password" />
+        <input type="password" name="confirmpassword" value="<?= $confirmpassword ?>" placeholder="Confirm Password" />
         <div class="form__control">
           <label for="avatar">User Avatar</label>
-          <input type="file" id="avatar" />
+          <input type="file" name="avatar" id="avatar" />
         </div>
-        <button type="submit" class="btn">Sign Up</button>
-        <small>Already have an account? <a href="signin.html">Sign In</a></small>
+        <button type="submit" name="submit" class="btn">Sign Up</button>
+        <small>Already have an account? <a href="signin.php">Sign In</a></small>
       </form>
     </div>
   </section>
