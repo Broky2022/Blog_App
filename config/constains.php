7064a22d9d8265646dcfbfd23211388315a663db
Broky2022 <nhktb2k3@gmail.com>
2025-04-03 01:03:30 +0700

    [ADD][constain.php & database.php] configure database connection settings

--- config/constains.php
+++ config/constains.php
@@ -1,3 +1,7 @@
 <?php
-    define('ROOT_URL', 'http://localhost/Blog_App/')
+    define('ROOT_URL', 'http://localhost/Blog_App/');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'admin'); // user sửa dụng để kết nối đến database
+    define('DB_PASS', 'admin123'); // password sửa dụng để kết nối đến database
+    define('DB_NAME', 'blog'); // tên database
 ?>
--- config/database.php
+++ config/database.php
@@ -1,3 +1,11 @@
 <?php
     require 'config/constains.php';
+
+    // kết nối database (địa chỉ từ constains.php)
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    // kiểm tra kết nối
+    if (mysqli_errno($conn)) {
+        die(mysqli_errno($conn));
+    }
 ?>
