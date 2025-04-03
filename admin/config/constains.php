8e00da64c07085474655d3ee4973d87e01622a12
Broky2022 <nhktb2k3@gmail.com>
2025-04-04 00:18:46 +0700

    [UPDATE][constains.php] khởi động session và thêm chú thích cho đường dẫn gốc

--- admin/config/constains.php
+++ admin/config/constains.php
@@ -1,7 +1,7 @@
 <?php
-    define('ROOT_URL', 'http://localhost/Blog_App/');
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'admin'); // user sửa dụng để kết nối đến database
-    define('DB_PASS', 'admin123'); // password sửa dụng để kết nối đến database
-    define('DB_NAME', 'blog'); // tên database
-?>
+session_start(); // khởi động session
+define('ROOT_URL', 'http://localhost/Blog_App/'); // đường dẫn đến thư mục gốc của ứng dụng tại htdocs
+define('DB_HOST', 'localhost');
+define('DB_USER', 'admin'); // user sửa dụng để kết nối đến database
+define('DB_PASS', 'admin123'); // password sửa dụng để kết nối đến database
+define('DB_NAME', 'blog'); // tên database
--- config/constains.php
+++ config/constains.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    define('ROOT_URL', 'http://localhost/Blog_App/');
+    define('ROOT_URL', 'http://localhost/Blog_App/'); // đường dẫn đến thư mục gốc của ứng dụng tại htdocs
     define('DB_HOST', 'localhost');
     define('DB_USER', 'admin'); // user sửa dụng để kết nối đến database
     define('DB_PASS', 'admin123'); // password sửa dụng để kết nối đến database
