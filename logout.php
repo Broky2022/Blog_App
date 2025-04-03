4e8b751ce91025f5212bb4668a738af6ed2d53c3
Broky2022 <nhktb2k3@gmail.com>
2025-04-04 00:19:21 +0700

    [ADD][logout.php] thêm file xử lý đăng xuất người dùng và chuyển hướng về homepage

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+    require 'config/constains.php';
+
+    // Xóa session người dùng và trở về home page
+    session_destroy();
+    // Xóa cookie
+    // if (isset($_COOKIE['user_id'])) {
+    //     setcookie('user_id', '', time() - 3600, '/');
+    // }
+    header('location: ' . ROOT_URL);
+    exit;
+?>
