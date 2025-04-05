144b9d7ac916b4a0407cb84a92185fda061c95eb
Broky2022 <nhktb2k3@gmail.com>
2025-04-05 23:40:24 +0700

    [ADD][add-user.php] cập nhật form thêm người dùng với thông tin đã nhập và thông báo lỗi

--- admin/add-user.php
+++ admin/add-user.php
@@ -1,30 +1,53 @@
 <?php
 include 'shares/header.php';
+
+// lấy lại thông tin đã nhập trong form khi bị lỗi
+$firstname = $_SESSION['add-user-data']['firstname'] ?? null;
+$lastname = $_SESSION['add-user-data']['lastname'] ?? null;
+$username = $_SESSION['add-user-data']['username'] ?? null;
+$email = $_SESSION['add-user-data']['email'] ?? null;
+$createpassword = $_SESSION['add-user-data']['createpassword'] ?? null;
+$confirmpassword = $_SESSION['add-user-data']['confirmpassword'] ?? null;
+
+// Xóa thông tin đã nhập trong form
+unset($_SESSION['add-user-data']);
 ?>
 
 <section class="form__section">
   <div class="container form__section-container">
     <h2>Add User</h2>
-    <form action="" enctype="multipart/form-data">
-      <input type="text" placeholder="First Name" />
-      <input type="text" placeholder="Last Name" />
-      <input type="text" placeholder="Username" />
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Create Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <select>
+    <!-- hiển thị thông báo lỗi nếu có -->
+    <?php if (isset($_SESSION['add-user'])) : ?>
+      <div class="alert__message error">
+        <p>
+          <?= $_SESSION['add-user'];
+          unset($_SESSION['add-user']); ?>
+        </p>
+      </div>
+    <?php endif; ?>
+
+    <form action="<?= ROOT_URL ?>controller/adduser-controller.php" enctype="multipart/form-data" method="POST">
+      <input type="text" name="firstname" value="<?= $firstname ?>" placeholder="First Name" />
+      <input type="text" name="lastname" value="<?= $lastname ?>" placeholder="Last Name" />
+      <input type="text" name="username" value="<?= $username ?>" placeholder="Username" />
+      <input type="email" name="email" value="<?= $email ?>" placeholder="Email" />
+      <input type="password" name="createpassword" value="<?= $createpassword ?>" placeholder="Create Password" />
+      <input type="password" name="confirmpassword" value="<?= $confirmpassword ?>" placeholder="Confirm Password" />
+      <!-- chỉ có admin mới có thể thêm người dùng admin -->
+      <select name="userrole" value="<?= $userrole ?>">
+        <option value="" selected disabled>Select Role</option>
         <option value="0">Author</option>
         <option value="1">Admin</option>
       </select>
       <div class="form__control">
         <label for="avatar">User Avatar</label>
-        <input type="file" id="avatar" />
+        <input type="file" name="avatar" id="avatar" />
       </div>
-      <button type="submit" class="btn">Add User</button>
+      <button type="submit" name="submit" class="btn">Add User</button>
     </form>
   </div>
 </section>
 
 <?php
-include './/shares/footer.php';
+include './shares/footer.php';
 ?>
