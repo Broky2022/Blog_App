dd4f154fb0b0c620cd7d1bc83a4194999932d622
Broky2022 <nhktb2k3@gmail.com>
2025-04-07 14:31:14 +0700

    [UPDATE][header.php] bỏ kiểm tra quyền admin trong menu điều hướng

--- admin/shares/header.php
+++ admin/shares/header.php
@@ -6,8 +6,8 @@ if (!isset($_SESSION['user-id'])) {
   header('location: ' . ROOT_URL . 'signin.php');
   exit;
 }
-if (!isset($_SESSION['user_is_admin']) || $_SESSION['user_is_admin'] !== true) {
-  header('location: ' . ROOT_URL . 'index.php');
-  exit;
-}
+// if (!isset($_SESSION['user_is_admin']) || $_SESSION['user_is_admin'] !== true) {
+//   header('location: ' . ROOT_URL . 'index.php');
+//   exit;
+// }
 ?>
